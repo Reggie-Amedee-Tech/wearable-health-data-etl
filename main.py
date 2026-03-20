945b4e1338f26ff336d61b515ddb509eafa70125
Jenelle Downer <jenelledowner@macbookair.mynetworksettings.com>
2026-03-20 04:21:08 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,111 @@
+def clean_heartrate_data(data: list) -> tuple:
+    cleaned_hr_data = []
+    num_rows_removed =0
+    # Filter Non- Digit Strings uses isdigit() method, adds number to cleaned_hr_data (), else removes it and stores in it removed[]
+    for j in data:
+        if j != None and j.isdigit(): 
+           cleaned_hr_data.append(int(j))
+        else:
+            num_rows_removed +=1
+    return cleaned_hr_data
+
+
+   
+
+
+def average(data: list) -> float:
+    
+    clean_data=data
+    total_hr = 0
+    for i in clean_data:
+        total_hr = total_hr + i
+    avg_hr =total_hr/len(clean_data)   
+    return round(avg_hr,2)
+     # test 
+     #print(f"This is the sum of hr : {total_hr}" ) 
+     # print(f" This is the average hr : {avg_hr}" ) 
+    
+
+
+def median(data: list) -> float:
+    
+    # 1. Sorting the list
+    sorted_list= sorted(data)
+    length= len(sorted_list)
+    #2. Calculate the median
+    if length % 2 != 0:
+        return sorted_list[length//2]
+    else:
+       return round((sorted_list[length//2] +sorted_list[length//2 -1]) //2, 2)
+
+
+    return sorted_list
+
+
+def range(data: list) -> float:
+    
+    sorted_list= sorted(data)
+    return sorted_list [-1] - sorted_list[0]
+
+def rolling_avg(data: list, k: int) -> float:
+ """""
+   clean_data=data
+   total_hr = 0
+   for i in clean_data:
+        if i <= k:
+            total_hr = total_hr + i
+        else:
+            return total_hr/k    
+
+    Was working through the challenge 
+    """
+ pass
+
+def run(file: str):
+    """
+    Process heart rate data from the a file by cleaning and
+    calculating summary statistics. Print out final values.
+
+    Args:
+        filename (str): The path to the data file (e.g., 'data/phase0.txt').
+
+    Returns:
+        float, float, float: You will return the average, median, and range.
+    """
+    data = []
+    cleaned_data = []
+    # open file using file I/O and read it into the `data` list
+    file_obj = open(file) 
+    data = file_obj.readlines()
+
+ 
+    # Use `clean_heartrate_data` to clean the data and remove invalid entries
+    #cleaned_list, removed_values = clean_heartrate_data (data)
+    #clean_hr_data = clean_heartrate_data(data)
+
+    for item in data:
+     cleaned_data.append(item.replace('\n', ''))
+
+    new_data = clean_heartrate_data(cleaned_data)
+   
+    #calculate the average, median, and range of this file using the functions you've wrote
+    average_hr = average(new_data)
+    median_hr= median(new_data)
+    range_hr = range (new_data)
+    #rolling_avg_hr = rolling_avg(clean_hr_data)
+
+    # print out your data quality measure to the console
+    print (f"Cleaned Heart Rate Data: {new_data}")
+
+    # print out your descriptive statistics to the console
+
+    print(f"Average Heart-Rate {average_hr}")
+    print(f"Median Heart-Rate {median_hr}")
+    print(f"Range Heart-Rate {range_hr}")
+    #print(f"Average Heart-Rate{rolling_avg_hr} + {int = 10}")
+
+if __name__ == "__main__":
+    run("data/phase0.txt")
+    run("data/phase1.txt")
+    run("data/phase2.txt")
+    run("data/phase3.txt")
---
+++ results/writeup.md
@@ -0,0 +1,20 @@
+1) Which file appears to represent the most active period? Explain using at least two metrics. Consider that this is a 30 year old participant and compare your output to the column titled "Target HR Zone 50-85%" within this link: https://www.heart.org/en/healthy-living/fitness/fitness-basics/target-heart-rates
+
+Phase 1 file respresents the most active period. The mean in this data is 87.3 which is the highest averaage of all the files. The median in the Phase 1 file also demonstrates this at a value of 88 which is the highest median compared to the other phases. Even though these values are considered 
+
+2) Which file had the **poorest** data quality? How do you know?
+
+The file that has the poorest data quality is Phase 3. The mean is = 60. 65 while ther median is 56, this signals that there might be a few outliers within the data set.
+
+3) Suppose one heart-rate file contains the following cleaned values: `68, 70, 71, 72, 72, 73, 74, 75, 180`. The value 180 was recorded during a sensor glitch.
+
+a) Calculate the range of this dataset.
+
+Range = 180 - 68 = 112
+
+b) Explain how the extreme value affects the range.
+
+ The extreme value affects the range by returning a large difference. This value tells us that the data is spread out. 
+
+c) Identify a different statistic that would better represent the typical variability of the dataset. Why would this measure be better?
+Standard deviation would be a better representation measure of the typical variability of this dataset, because it tells you how far values are from the middle of dataset; giving a better understanding of how spread out the values are. 
